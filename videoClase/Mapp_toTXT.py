dc3a37b533fcabc0934e995c617ba7b2ce246a0d
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2023-10-25 23:54:37 -0300

    Funco

--- videoClase/Mapp_toTXT.py
+++ videoClase/Mapp_toTXT.py
@@ -1,12 +1,15 @@
+# Funcional.
+# Enlista las palabras según su linea correspondiente.
+
 try:
-    with open("../Biobio/biobio_pages.txt", "r", encoding="utf-8") as archivo, \
-         open("../Biobio/Mapped-Biobio.txt", "a", encoding="utf-8") as f:
+    with open("Biobio/biobio_pages.txt", "r", encoding="utf-8") as archivo, \
+         open("Biobio/mapped-Biobio.txt", "a", encoding="utf-8") as f:
         for num_linea, linea in enumerate(archivo, start=1):
             palabras = linea.lower().split()
             for palabra in palabras:
                 f.write(f"{palabra} {num_linea}\n")
 except FileNotFoundError:
-    print("Error: Could not find the specified file.")
+    print("Error: No se pudo encontrar el archivo especificado.")
 except PermissionError:
-    print("Error: You do not have permission to access the specified file.")
+    print("Error: No tienes permiso para acceder al archivo especificado.")
 
